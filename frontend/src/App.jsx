65afe8b0f847e2a7fb3a882e555f63ce138493b1
Brendan Wong <bjwpokemon@gmail.com>
2023-04-15 19:31:06 -0400

    Current Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import { Transition, Menu} from '@headlessui/react'
+import { useState, Fragment} from "react";
+import { Transition, Menu, Dialog} from '@headlessui/react'
 import classNames from 'classnames';
 import "./App.css";
 function App() {
 
   const [fromToken, setFromToken] = useState('SOL');
   const [toToken, setToToken] = useState('USDC');
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleFromTokenChange = (value) => {
     setFromToken(value);
@@ -153,11 +154,80 @@ function App() {
               <span className="token-price-label">Price</span>
               <span className="token-price-value">${fromToken} 1 = ${toToken} 42.1234</span>
             </div>
-              <button className="token-swap-button bg-green hover:bg-green-700 rounded-lg text-white py-2 px-8">
+              <button className="token-swap-button bg-green hover:bg-green-700 rounded-lg text-white py-2 px-8"
+                onClick={() => {
+                  setOpenDialog(true);
+                }}>
                 Swap
               </button>
             </div>
           </div>
+          <Transition appear show={openDialog} as={Fragment}>
+        <Dialog as="div" onClose={() => {
+          setOpenDialog(false);
+        }}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Confirm Swap
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Confirm swap of 1.2345 {fromToken} for 42.1234 {toToken}
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        setOpenDialog(false);
+                      }}
+                    >
+                      Swap
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        setOpenDialog(false);
+                      }}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
       </div>
     </div>
   );
