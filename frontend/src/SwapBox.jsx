073e01c26de702e38a3c1e4cab6e4db4fd1ff606
Brendan Wong <bjwpokemon@gmail.com>
2023-04-16 01:38:38 -0400

    TRANSITION WOOO

--- frontend/src/SwapBox.jsx
+++ frontend/src/SwapBox.jsx
@@ -3,6 +3,9 @@ import { Transition, Menu, Dialog } from "@headlessui/react";
 import Modal from 'react-modal'
 import "./SwapBox.css"
 import classNames from 'classnames';
+import { Transaction, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import axios from 'axios';
 
 import TokenDropdown from "./TokenDropdown";
 import TokenSet from "./TokenSet";
@@ -12,25 +15,47 @@ Modal.setAppElement('#root');
  * triggerSwap: ([token1, token2, token1qty])
  */
 const SwapBox = () => {
-  const [fromToken, setFromToken] = useState("SOL");
-  const [toToken, setToToken] = useState("USDC");
+  const [fromToken, setFromToken] = useState("");
+  const [toToken, setToToken] = useState("");
   const [openSwapDialog, setSwapOpenDialog] = useState(false);
   const [openDepositDialog, setDepositOpenDialog] = useState(false);
+  const [openWithdrawDialog, setWithdrawOpenDialog] = useState(false);
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const depositAPIURL = "/http://localhost:4000/swap/deposit"
 
   const handleFromTokenChange = (value) => {
     setFromToken(value);
   };
+
+  const handleToTokenChange = (value) => {
+    setToToken(value);
+  };
+
   const fromTokenOptions = [
     { value: "SOL", label: "SOL" },
   ];
 
+  async function sendSol(amount) {
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: "4Si9336CP4jyEuDsM5aB2jnTnyAbxyN25mvy8xbvxbbR",
+        lamports: amount,
+      })
+    );
+    await sendTransaction(transaction, connection);
+  }
+
   return (
     <>
       <div className="token-swap-container">
-          <span className="token-swap_label">Amount to Swap</span>
-          <TokenDropdown options={fromTokenOptions} value={fromToken} setValue={handleFromTokenChange} />
-          <span className="token-swap_label">SolStash™ Composition</span>
-          <TokenSet options={fromTokenOptions} value={fromToken} setValue={handleFromTokenChange}/>
+        <span className="token-swap_label">Amount to Swap</span>
+        <TokenDropdown options={true} value={fromToken} setValue={handleFromTokenChange} />
+        <span className="token-swap_label">SolStash™ Composition</span>
+        <TokenSet options={fromTokenOptions} value={toToken} setValue={handleToTokenChange} />
+        <div className="button-container">
           <button
             className="token-swap-button hover:bg-green-700 rounded-lg text-white py-2 px-8"
             onClick={() => {
@@ -39,6 +64,24 @@ const SwapBox = () => {
           >
             Invest
           </button>
+        </div>
+      </div>
+
+      <div className="token-swap-container">
+        <span className="token-swap_label">Amount to Withdraw</span>
+        <TokenDropdown options={false} value={toToken} setValue={handleToTokenChange} />
+        <span className="token-swap_label">SolStash™ Composition</span>
+        <TokenSet options={fromTokenOptions} value={fromToken} setValue={handleFromTokenChange} />
+        <div className="button-container">
+          <button
+            className="token-swap-button hover:bg-green-700 rounded-lg text-white py-2 px-8"
+            onClick={() => {
+              setWithdrawOpenDialog(true);
+            }}
+          >
+            Withdraw
+          </button>
+        </div>
       </div>
 
       <Transition appear show={openSwapDialog} as={Fragment}>
@@ -81,7 +124,7 @@ const SwapBox = () => {
                       Receive: 0.01 SOL, 0.01 USDC, 0.01 SRM
                     </p>
                     <p className="text-sm text-red-500">
-                      Send: 1 SOL
+                      Send: {+fromToken} SOL
                     </p>
                     <br></br>
                     <p className="text-sm text-gray-500">
@@ -89,24 +132,26 @@ const SwapBox = () => {
                     </p>
                   </div>
 
-                  <div className = "mt-4 modal-button-container">
-                      <button
-                        type="button"
-                        className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                        onClick={() => {
-                          setSwapOpenDialog(false);
-                          setDepositOpenDialog(true);
-                        }}
-                      >
-                        Swap
-                      </button>
-                      <button
-                        type="button"
-                        className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                        onClick={() => setSwapOpenDialog(false)}
-                      >
-                        Cancel
-                      </button>
+                  <div className="mt-4 modal-button-container">
+                    <button
+                      type="button"
+                      className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={async () => {
+                        // let response = await sendSol(+fromToken * LAMPORTS_PER_SOL)
+                        // console.log(response)
+                        setSwapOpenDialog(false);
+                        setDepositOpenDialog(true);
+                      }}
+                    >
+                      Swap
+                    </button>
+                    <button
+                      type="button"
+                      className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setSwapOpenDialog(false)}
+                    >
+                      Cancel
+                    </button>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -114,6 +159,7 @@ const SwapBox = () => {
           </div>
         </Dialog>
       </Transition>
+
       <Transition appear show={openDepositDialog} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={() => setDepositOpenDialog(false)}>
           <Transition.Child
@@ -163,7 +209,12 @@ const SwapBox = () => {
                     <button
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={() => {
+                      onClick={async () => {
+                        // let response = await axios.post(depositAPIURL, {
+                        //   "address": publicKey,
+                        //   "amount": +fromToken * LAMPORTS_PER_SOL,
+                        // })
+                        // console.log(response.data);
                         setDepositOpenDialog(false);
                       }}
                     >
@@ -173,6 +224,79 @@ const SwapBox = () => {
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={() => setDepositOpenDialog(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <Transition appear show={openWithdrawDialog} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => setWithdrawOpenDialog(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Confirm Transaction Details
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Estimated Changes
+                    </p>
+                    <p className="text-sm text-green-500">
+                      Receive: 1 SOL
+                    </p>
+                    <p className="text-sm text-red-500">
+                      Send: {+toToken} STH
+                    </p>
+                    <br></br>
+                    <p className="text-sm text-gray-500">
+                      Network Fee: $0.00012
+                    </p>
+                  </div>
+
+                  <div className="mt-4 modal-button-container">
+                    <button
+                      type="button"
+                      className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        setWithdrawOpenDialog(false);
+                      }}
+                    >
+                      Withdraw
+                    </button>
+                    <button
+                      type="button"
+                      className="rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setWithdrawOpenDialog(false)}
                     >
                       Cancel
                     </button>
--- frontend/src/TokenDropdown.jsx
+++ frontend/src/TokenDropdown.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { Transition, Menu, Dialog, Listbox } from "@headlessui/react";
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 import "./TokenDropdown.css";
 
-const TokenDropdown = ({ options, value, setValue }) => {
+const TokenDropdown = ({ options, value: enteredValue, setValue: setEnteredValue }) => {
   //conv rate: 24.48
 
-  const [enteredValue, setEnteredValue] = useState("");
   const [isFocused, setFocus] = useState(false);
 
   const { connection } = useConnection();
@@ -16,20 +16,29 @@ const TokenDropdown = ({ options, value, setValue }) => {
   useEffect(() => {
     (async  () => {
       let balance = await connection.getBalance(publicKey);
-      setBalance(balance);
+      setBalance(balance / LAMPORTS_PER_SOL);
     })();
   }, [publicKey]);
 
-  const inUSD = ((+enteredValue || 0) * 24.48).toFixed(2);
+  const solInUSD = ((+enteredValue || 0) * 24.48).toFixed(2);
+  const sthInUSD = ((+enteredValue || 0) * 82.50).toFixed(2);
 
   return (
-    <div className={`token-dropdown ${isFocused ? "token-dropdown-focused" : ""}`}>
+    <>
+    {options && (
+      <div className={`token-dropdown ${isFocused ? "token-dropdown-focused" : ""}`}>
       <input
         className="token-dropdown_amount"
         type="text"
         placeholder="0.00"
         value={enteredValue}
-        onChange={(e) => setEnteredValue(e.target.value)}
+        onChange={(e) => {
+          if (e.target.value > balance) {
+            setEnteredValue(balance);
+          } else {
+            setEnteredValue(e.target.value)}
+        }
+        }
         onFocus={() => setFocus(true)}
         onBlur={() => setFocus(false)}
       />
@@ -37,17 +46,52 @@ const TokenDropdown = ({ options, value, setValue }) => {
       <div className="token-dropdown_button">
         <img
           className="token-menu-icon"
-          src={`/tokens/${value}.png`}
-          alt={`${value} icon`}
+          src={`/tokens/SOL.png`}
+          alt={`SOL icon`}
         />
-        <span>{value}</span>
+        <span>SOL</span>
       </div>
 
       
-      <span className="token-dropdown_stack_bottom token-dropdown_left">≈ ${inUSD} USD</span>
+      <span className="token-dropdown_stack_bottom token-dropdown_left">≈ ${solInUSD} USD</span>
 
-      <span className="token-dropdown_stack_bottom token-dropdown_right">Up To {balance} SOL</span>
+      <span className="token-dropdown_stack_bottom token-dropdown_right">Up To {balance.toFixed(2)} SOL</span>
     </div>
+    )}
+    {!options && (
+      <div className={`token-dropdown ${isFocused ? "token-dropdown-focused" : ""}`}>
+      <input
+        className="token-dropdown_amount"
+        type="text"
+        placeholder="0.00"
+        value={enteredValue}
+        onChange={(e) => {
+          if (e.target.value > balance) {
+            setEnteredValue(balance);
+          } else {
+            setEnteredValue(e.target.value)}
+        }
+        }
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+      />
+
+      <div className="token-dropdown_button">
+        <img
+          className="token-menu-icon"
+          src={`/tokens/SOL.png`}
+          alt={`SOL icon`}
+        />
+        <span>STH</span>
+      </div>
+
+      
+      <span className="token-dropdown_stack_bottom token-dropdown_left">≈ ${sthInUSD} USD</span>
+
+      <span className="token-dropdown_stack_bottom token-dropdown_right">Up To {balance.toFixed(2)} STH</span>
+    </div>
+    )}
+    </>
   );
 };
 
