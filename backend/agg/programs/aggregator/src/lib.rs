a3313598788d1623b9cb916f3020186c0ba1ee89
Timothy G <timg51237@gmail.com>
2023-04-15 21:47:23 -0400

    this doesn't work :(

--- backend/agg/programs/aggregator/src/lib.rs
+++ backend/agg/programs/aggregator/src/lib.rs
@@ -1,34 +1,78 @@
 use anchor_lang::prelude::*;
+use whirlpool::cpi::account::Swap
+use whirlpool::program::Whirlpool
+use whirlpool::cpi::swap
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod puppet {
+pub mod aggregator {
     use super::*;
     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
-        let puppet = &mut ctx.accounts.puppet;
-        puppet.data = data;
+    pub fn test_swap(
+        ctx: Context<OrcaSwap>,
+        amount: u64,
+        other_amount_threshold: u64,
+        sqrt_price_limit: u128,
+        amount_specified_is_input: bool,
+        a_to_b: bool,
+    ) -> Result<()> {
+        let cpi_program = ctx.accounts.whirlpool_program.to_account_info();
+        let cpi_accounts = Swap {
+            puppet: ctx.accounts.puppet.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        puppet::cpi::set_data(cpi_ctx, data)
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub puppet: Account<'info, Data>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct SetData<'info> {
+pub struct OrcaSwap<'info> {
+    pub whirlpool_program: Program<'info, Whirlpool>,
+    
+    #[account(address = token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    pub token_authority: Signer<'info>,
+
     #[account(mut)]
-    pub puppet: Account<'info, Data>,
+    pub whirlpool: Box<Account<'info, Whirlpool>>,
+
+    #[account(mut, constraint = token_owner_account_a.mint == whirlpool.token_mint_a)]
+    pub token_owner_account_a: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_a)]
+    pub token_vault_a: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut, constraint = token_owner_account_b.mint == whirlpool.token_mint_b)]
+    pub token_owner_account_b: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_b)]
+    pub token_vault_b: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_0: AccountLoader<'info, TickArray>,
+
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_1: AccountLoader<'info, TickArray>,
+
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_2: AccountLoader<'info, TickArray>,
+
+    #[account(seeds = [b"oracle", whirlpool.key().as_ref()],bump)]
+    /// CHECK: Oracle is currently unused and will be enabled on subsequent updates
+    pub oracle: UncheckedAccount<'info>,
+
 }
 
 #[account]
